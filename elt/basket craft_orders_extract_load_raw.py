44ef46edf4a485223db306b7d48cb11c0075346b
Lucaszhuokaiyu <112446521+Lucaszhuokaiyu@users.noreply.github.com>
2025-04-28 04:25:37 +0000

    a

--- elt/basket craft_orders_extract_load_raw.py
+++ elt/basket craft_orders_extract_load_raw.py
@@ -24,12 +24,13 @@ pg_conn_str = f'postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_db}'
 mysql_engine = create_engine(mysql_conn_str)
 pg_engine = create_engine(pg_conn_str)
 # %%
-# Read products table from MySQL and load into a DataFrame
-df = pd.read_sql('SELECT * FROM products', mysql_engine)
+# Read orders table from MySQL and load into a DataFrame
+df = pd.read_sql('SELECT * FROM orders', mysql_engine)
 # %%
 # Write DataFrame to orders table in postgres
-df.to_sql('products', pg_engine, schema = 'raw', if_exists='replace', index=False)
+df.to_sql('orders', pg_engine, schema = 'raw', if_exists='replace', index=False)
 # %%
-print(f'{len(df)} records loaded into postgres products table')
+print(f'{len(df)} records loaded into postgres orders table')
 # %%
 df
+# %%
