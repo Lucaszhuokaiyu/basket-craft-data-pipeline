3fbf52bbab522e3a7796482ca7e4fb76c8878d52
Lucaszhuokaiyu <112446521+Lucaszhuokaiyu@users.noreply.github.com>
2025-05-05 20:21:46 +0000

    a

---
+++ basket_craft_dbt/models/staging/stg_website_sessions.sql
@@ -0,0 +1,16 @@
+WITH source AS (
+    SELECT * FROM raw.website_sessions
+)
+
+SELECT
+    website_session_id,
+    user_id,
+    utm_campaign,
+    utm_content,
+    utm_source,
+    device_type,
+    http_referrer,
+    is_repeat_session,
+    created_at AS website_session_created_at,
+    CURRENT_TIMESTAMP AS loaded_at
+FROM source
---
+++ basket_craft_dbt/models/warehouse/fct_website_sessions_utm_source_daily.sql
@@ -0,0 +1,9 @@
+SELECT
+    DATE(website_session_created_at) AS website_session_day,
+    utm_source,
+    COUNT(website_session_id) AS sessions,
+    SUM(is_repeat_session)::INT AS repeat_sessions,
+    (SUM(is_repeat_session)::NUMERIC / COUNT(website_session_id)) * 100 AS repeat_sessions_pct
+FROM {{ ref('stg_website_sessions') }}
+GROUP BY 1, 2
+ORDER BY 1
