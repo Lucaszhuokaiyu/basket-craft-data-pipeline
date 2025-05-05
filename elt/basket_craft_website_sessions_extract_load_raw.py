5562998e41d802a6b9ce10f59f7830af205e2cc8
Lucaszhuokaiyu <112446521+Lucaszhuokaiyu@users.noreply.github.com>
2025-05-05 18:35:33 +0000

    added website_sessions extract load raw

---
+++ elt/basket_craft_website_sessions_extract_load_raw.py
@@ -0,0 +1,38 @@
+# %%
+import pandas as pd
+from sqlalchemy import create_engine
+
+# %%
+# MySQL database connection detail
+mysql_user = 'analyst'
+mysql_password = 'go_lions'
+mysql_host = 'db.isba.co'
+mysql_db = 'basket_craft'
+
+# Postgres database connection detail
+pg_user = 'postgres'
+pg_password = 'isba_4715'
+pg_host = 'isba-dev-02.cmb4w8cmqb26.us-east-1.rds.amazonaws.com'
+pg_db = 'basket_craft'
+
+# %%
+# Build connection strings
+mysql_conn_str = f'mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}'
+pg_conn_str = f'postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_db}'
+
+# %%
+# Create database engines
+mysql_engine = create_engine(mysql_conn_str)
+pg_engine = create_engine(pg_conn_str)
+
+# %%
+df = pd.read_sql('SELECT * FROM website_sessions', mysql_engine)
+
+# %%
+df.to_sql('website_sessions', pg_engine, schema = 'raw', if_exists='replace', index=False)
+
+# %%
+print(f'{len(df)} records loaded into postgres website_sessions table')
+# %%
+df
+# %%
